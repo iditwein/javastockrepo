c46eb895d6b72f80a83d4999751d3b432b9e4b05
iditwein <iditwein@gmail.com>
2014-12-25 18:43:26 +0200

    Exercise 2

--- src/com/myorg/javacourse/IditexServlet.java
+++ src/com/myorg/javacourse/IditexServlet.java
@@ -5,7 +5,13 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class IditexServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html");
+		int num1=4;
+		int num2=7;
+		int num3=3;
+		int result=(num1+num3)*num2;
+		String resultStr=new String("<h1>Result of "+"("+num1+"+"+num3+")"+"*"+num2+"="+result+"</h1>");
+		resp.getWriter().println(resultStr);
+		
 	}
 }
